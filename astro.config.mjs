b6944497b86fc3709a1a22e2ac4d190e0364586c
Bhavya Gupta <bhavya.gupta1@sprinklr.com>
2026-05-25 11:38:30 +0530

    github pages deployment setup
    Setting up all of the files and folder to set up github pages deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://bhavya-gutpa1.github.io',
+    base: '/Intern_Log',
+});
